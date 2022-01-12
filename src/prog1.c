b1c6f6b21c7fc06154820a366723f0fe29d16f30
grechnikovartem <91223086+grechnikovartem@users.noreply.github.com>
2022-01-12 19:36:18 +0300

    Create prog1.c

---
+++ src/prog1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "functions.h"
+
+int main(){
+    int c, x, y;
+    float a, b;
+    while(scanf("%d", &c) > 0){
+        if(c == 1){
+            if(scanf("%d %d", &x, &y) == 2)
+                printf("GCF(%d, %d) = %d\n", x, y, GCF(x, y));
+        }
+        else if(c == 2){
+            if(scanf("%f %f", &a, &b) == 2)
+                printf("Square = %f\n", Square(a, b));
+        }
+        else
+            printf("Incorrect comand!\n");
+    }
+}
